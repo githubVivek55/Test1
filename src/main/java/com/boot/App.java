f0d9080797463131fa9be170d7a9bf20bf7edd0c
githubVivek55 <vivekpce55@outlook.com>
2017-05-28 12:35:46 +0530

    testing git

--- src/main/java/com/boot/App.java
+++ src/main/java/com/boot/App.java
@@ -1,17 +1,13 @@
 package com.boot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * Hello world!
  *
  */
-@SpringBootApplication(scanBasePackages={"com.boot"})
-public class App
+public class App 
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(App.class,args);
+        System.out.println( "Hello World!" );
     }
 }
